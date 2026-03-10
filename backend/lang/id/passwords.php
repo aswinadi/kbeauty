86fc0ef68c64c6ca5b3a380287f2bdc23bbeb18d
aswinadi <aswin.adisaputra@gmail.com>
2026-03-10 15:20:58 +0700

    feat: add indonesian translations for backend

---
+++ backend/lang/id/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'Kata sandi Anda telah disetel ulang!',
+    'sent' => 'Kami telah mengirimkan email tautan penyetelan ulang kata sandi Anda!',
+    'throttled' => 'Harap tunggu sebelum mencoba lagi.',
+    'token' => 'Token penyetelan ulang kata sandi tidak sah.',
+    'user' => 'Kami tidak dapat menemukan pengguna dengan alamat email tersebut.',
+];
