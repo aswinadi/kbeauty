7585b8f084253fcd06259b6f3436ec716a07a6cc
aswinadi <aswin.adisaputra@gmail.com>
2026-03-11 13:47:37 +0700

    Optimize for Android 15: Support 16 KB page sizes and Target SDK 35

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -10,8 +10,14 @@ import java.io.FileInputStream
 
 android {
     namespace = "com.example.mobile"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
+
+    packaging {
+        jniLibs {
+            useLegacyPackaging = false
+        }
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -28,7 +34,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
