4d49f1b4db42d890cdd1e4556a5598343de7c70a
aswinadi <aswin.adisaputra@gmail.com>
2026-03-22 14:17:28 +0700

    Fix Android build namespace error for older plugins

--- mobile/android/build.gradle.kts
+++ mobile/android/build.gradle.kts
@@ -19,6 +19,17 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    afterEvaluate {
+        if (plugins.hasPlugin("com.android.library") || plugins.hasPlugin("com.android.application")) {
+            val android = extensions.getByName("android") as com.android.build.gradle.BaseExtension
+            if (android.namespace == null) {
+                android.namespace = "id.antigravity.${project.name.replace("-", ".").replace("_", ".")}"
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
