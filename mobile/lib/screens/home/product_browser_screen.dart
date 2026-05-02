b540854192d661964fdca33221d9ad0aa4cc250f
aswinadi <aswin.adisaputra@gmail.com>
2026-05-02 13:15:10 +0700

    fix: resolve syntax errors in mobile app

--- mobile/lib/screens/home/product_browser_screen.dart
+++ mobile/lib/screens/home/product_browser_screen.dart
@@ -268,6 +268,7 @@ class _ProductBrowserScreenState extends State<ProductBrowserScreen> {
                                       ),
                                     ],
                                   ),
+                                  ),
                                 ),
                               ),
                             );
--- mobile/lib/widgets/product_card.dart
+++ mobile/lib/widgets/product_card.dart
@@ -127,6 +127,7 @@ class ProductCard extends StatelessWidget {
               ),
             ),
           ],
+          ),
         ),
       ),
     );
